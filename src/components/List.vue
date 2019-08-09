5c8c88e00d177f5f9560dd6b8d1a17721844641c
Jaiden DeChon <jaiden.dechon@gmail.com>
2019-08-09 13:14:09 -0500

    Switched to vue-persistent-state instead of manual localStorage or vuex

--- src/components/List.vue
+++ src/components/List.vue
@@ -2,169 +2,83 @@
 <template lang="pug">
 	div(class="list-container")
 
-		p(class="hint") Double-tap to activate. Swipe to remove. 
+		p(class="hint") 
+			i(class="fas fa-check") 
+			| to mark finished.  
+			i(class="fas fa-times") 
+			| to remove. 
 
 		ul(class="cards-container")
 
-			li(class="list-item")
-				div(class="card fade shadow")
-					p This is an item on the to-do list. 
-					i(class="fas fa-times fade shadow")
-					i(class="fas fa-check fade shadow")
+			li(v-for="todo in todos" class="list-item")
+				div(class="card shadow" :class="{ done: todo[1] == 'inactive' }")
+					p {{ todo[0] }}
+					i(@click="remove" class="fas fa-times shadow")
+					i(@click="toggle" class="fas fa-check shadow")
 
-			li(class="list-item")
-				div(class="card fade shadow")
-					p This is another item, but it's a little longer. The text should just wrap around and stretch the card. 
-					i(class="fas fa-times fade shadow")
-					i(class="fas fa-check fade shadow")
+			//- li(class="list-item")
+			//- 	div(class="card fade shadow")
+			//- 		p This is another item, but it's a little longer. The text should just wrap around and stretch the card. 
+			//- 		i(class="fas fa-times fade shadow")
+			//- 		i(class="fas fa-check fade shadow")
 
-			li(class="list-item")
-				div(class="card fade shadow done")
-					p This task is finished, which is why it looks like this. 
-					i(class="fas fa-times fade shadow")
-					i(class="fas fa-check fade shadow")
+			//- li(class="list-item")
+			//- 	div(class="card fade shadow done")
+			//- 		p This task is finished, which is why it looks like this. 
+			//- 		i(class="fas fa-times fade shadow")
+			//- 		i(class="fas fa-check fade shadow")
 
-			li(class="list-item")
-				div(class="card fade shadow")
-					p Here's another task that still has yet to be done. Maybe it'll get done soon?
-					i(class="fas fa-times fade shadow")
-					i(class="fas fa-check fade shadow")
+			//- li(class="list-item")
+			//- 	div(v-on:click="test" class="card fade shadow")
+			//- 		p Here's another task that still has yet to be done. Maybe it'll get done soon?
+			//- 		i(class="fas fa-times fade shadow")
+			//- 		i(class="fas fa-check fade shadow")
 
 </template>
 
 <script>
 
+// var fillList = require('../assets/js/fillList.js')
+// var handleCardDelete = require('../assets/js/handleCardDelete.js')
+// var handleCardToggle = require('../assets/js/handleCardToggle.js')
+
 	export default {
 
 		name: 'List',
-		mounted: function () {
 
-			let cardCloseButton = $('.fa-times')
-			let cardToggleButton = $('.fa-check')
+		methods: {
+			remove: function(theElementClicked) {
+				let thisElement = theElementClicked.target.parentNode
+				let thisCardsContent = theElementClicked.target.parentElement.children[0].innerHTML
 
-			// When close button is checked
-				
-				// Define this card's index
-
-				// get localStorage
-				// convert todo to object
-
-				// for item in todos
-
-					// if item index == cards index
-
-						// delete this item
-
-				// convert object back into string
-				// set localStorage
-
-				// rerun fillList()
-
-			// ___________________________________________________________________
-
-			// When toggle button is checked
-
-				// Define this card's index
-
-				// get todo from localStorage
-				// convert todo to object
-
-				// for item in todos
-
-					// if item index == cards index
-
-						// if item[1] == "active"
-
-							// item[1] == "inactive"
-
-						// if item[1] == "inactive"
-
-							// item[1] == "active"
-
-				// convert object back into string
-				// set localStorage
-
-				// rerun fillList()
-
-			// Populate cards-container with entries from LocalStorage
-			function fillList() {
-
-				// Find and remove all current cards
-				$(".list-item").remove();
-
-				// Define parent for housing cards
-				let cardsParent = $(".cards-container")
-
-				// Get todo contents form localStorage, convert to object
-				let todos = localStorage.getItem("todos")
-				let todosObject = JSON.parse(todos)
-
-				// Define function for creating cards
-				function makeCards (content, status, toDoIndex) {
-
-					// Define card attributes
-					let cardContent = content 
-					let cardStatus = status
-
-					// Create li to house card, append to parent
-					let li = document.createElement("li")
-					li.classList.add("list-item")
-					cardsParent.append(li)
-
-					// Create card element
-					let card = document.createElement("div")
-					card.classList.add("card", "fade", "shadow")
-					if (status != "active") {            // if status variable is not 'active', 
-						card.classList.add("done")       // apply class "done" to card to change styling
+				for (let todo in this.todos) {
+					let currentTodo = this.todos[todo]
+					if (thisCardsContent == currentTodo[0]) {
+						this.todos.splice(currentTodo[0], 1)
 					}
-					li.append(card)
+				}
+			},
+			toggle: function(theElementClicked) {
+				let thisElement = theElementClicked.target.parentNode
+				let thisCardsContent = theElementClicked.target.parentElement.children[0].innerHTML
 
-					// Create p element for content, append to card
-					let p = document.createElement("p")
-					let pContent = document.createTextNode(content)
-					p.appendChild(pContent)
-					card.append(p)
-
-					// Create and append card's close-button
-					let times = document.createElement("i")
-					times.classList.add("fas", "fa-times", "fade", "shadow")
-					card.append(times)
-
-					// Create and append card's check-button
-					let check = document.createElement("i")
-					check.classList.add("fas", "fa-check", "fade", "shadow")
-					card.append(check)
-
-					// Set index so that "status" (active/inactive) variable can be changed
-					// ... and saved so it persists between page reloads
-					card.setAttribute('data-index', toDoIndex)
-
+				for (let i in this.todos) {
+					let currentTodo = this.todos[i]
+					if (thisCardsContent == currentTodo[0]) {
+						let newStatus = "inactive"
+						currentTodo[1] = newStatus
+						let newTodo = currentTodo
+						this.todos[i] = newTodo
+					}
 				}
 
-				if (todos) {
-
-					// // For todo in todos...
-					for (var index = 0; index < todosObject.length; index++) {
-
-						console.log(index)
-
-						// Create variable out of todo
-						let entry = todosObject[index]
-
-						let content = entry[0]
-						let status = entry[1]
-						let toDoIndex = index
-
-						makeCards(content, status, toDoIndex)
-
-					}
-
-				}
-
+				let newtodolist = this.todos
+				this.todos = ''
+				this.todos = newtodolist
 			}
-			fillList()
+		},
 
-		}
+		mounted: function () {}
 	}
 </script>
 
@@ -201,6 +115,10 @@
 			margin: 0
 			padding: 40px 0 0 0
 
+			i
+
+				margin: 0 4px
+
 		.card
 
 			padding: 20px 80px 20px 40px
@@ -215,9 +133,25 @@
 
 			position: relative
 
+			overflow: none
+
 			display: flex
 			align-items: center
 			justify-content: center
+
+			transition: all .3s ease-in-out
+
+			&.hide
+
+				// width: 0
+				height: 0
+				margin: 0
+				padding: 0
+				opacity: 0
+
+				.fas
+
+					border: solid red
 
 			&.done
 
@@ -240,6 +174,11 @@
 			p
 
 				width: 100%
+
+			.fas
+
+				transition: all .3s ease-in-out
+				overflow: none
 
 			.fa-times, .fa-check
 
