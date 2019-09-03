edae6d01d903b2dd61efed25293192286d2b53a9
Jaiden DeChon <jaiden.dechon@gmail.com>
2019-09-03 05:01:34 -0500

    Added boilerplate, views

---
+++ src/assets/boilerplate.vue
@@ -0,0 +1,36 @@
+
+<template lang="pug"></template>
+
+<script>
+	export default {
+		name: ""
+	}
+</script>
+
+<style scoped lang="sass">
+
+	// smartphones, portrait iPhone, portrait 480x320 phones (Android)
+	// @media (min-width: 280px)
+
+	// Landscape iPhones
+	// @media (min-width: 568px)
+
+	// Portrait iPad
+	// @media (min-width: 750px)
+
+	// Landscape standard tablets, lo-res laptops and desktops
+	// @media (min-width: 801px)
+
+	// For laptops and desktops, and landscape big tablets
+	// @media (min-width: 1000px)
+
+	// For strange new 1440p laptops
+	// @media (min-width: 1440px)
+
+	// Full HD laptops & desktops, big tablets
+	// @media (min-width: 1920px)
+
+	// 4K screens
+	// @media (min-width: 1400px)
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<!-- TO DO -->
+<!-- Replace that HelloWorld component below with homepage components -->
+<!-- Such as component for creating new to-do lists, etc -->
+
+<template>
+  <div class="home">
+    <!-- <HelloWorld /> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
